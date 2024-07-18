b2310fa864b595d25a70e315ce092f2854a7c2e3
Roxy <155686159+RoxyKeatings@users.noreply.github.com>
2024-07-18 10:30:16 -0400

    imported axios, incorporated SheCodes dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
@@ -8,6 +9,14 @@ export default function Dictionary() {
     event.preventDefault();
     alert(`searching for definition of ${keyword}...`);
   }
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  let apiKey = "89b05tfca20b16d5f5e3c646e1oa37db";
+  // documentation:https://www.shecodes.io/learn/apis/dictionary
+  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(handleResponse);
 
   function handleKeywordchange(event) {
     setKeyword(event.target.value);
