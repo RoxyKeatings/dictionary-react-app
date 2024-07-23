ed83029c4eadc876bf0109e122c2200ae7f3738a
Roxy <155686159+RoxyKeatings@users.noreply.github.com>
2024-07-23 14:40:51 -0400

    changed conditional statements

--- src/Antonyms.js
+++ src/Antonyms.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./Antonyms.css";
 
 export default function Antonyms(props) {
-  if (props.antonyms.length >= 1) {
+  if (props.antonyms.length === null) {
+    return null;
+  } else {
     return (
       <div className="Antonyms">
         <h6>antonym(s): {props.antonyms}</h6>
       </div>
     );
-  } else {
-    return null;
   }
 }
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,16 +2,20 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  return (
-    <div className="Phonetic">
-      <span className="text">{props.phonetic.text}</span>
-      <a
-        href={props.phonetic.audio}
-        target="_blank"
-        rel="noreferrer"
-        className="listen">
-        Listen 🔊
-      </a>
-    </div>
-  );
+  if (props.phonetic.audio === null) {
+    return null;
+  } else {
+    return (
+      <div className="Phonetic">
+        <span className="text">{props.phonetic.text}</span>
+        <a
+          href={props.phonetic.audio}
+          target="_blank"
+          rel="noreferrer"
+          className="listen">
+          Listen 🔊
+        </a>
+      </div>
+    );
+  }
 }
